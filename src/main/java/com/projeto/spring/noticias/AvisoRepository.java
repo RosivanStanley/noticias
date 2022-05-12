31bcbb53420a482cc6ab5c34976f7647c0bcb6e2
RosivanStanley <rosivanstanley@hotmail.com>
2022-05-12 11:50:28 -0300

    Subindo projeto

---
+++ src/main/java/com/projeto/spring/noticias/AvisoRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.spring.noticias;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "api", path = "api")
+public interface AvisoRepository extends PagingAndSortingRepository<Aviso, Long> {
+
+}
